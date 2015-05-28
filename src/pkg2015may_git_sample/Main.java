8ce765d4c65339078ab20d17180448f6b2a21d41
riokarashigata <kekkou.komakai@gmail.com>
2015-05-28 11:02:40 +0900

    printer メソッドを追加した。

--- src/pkg2015may_git_sample/Main.java
+++ src/pkg2015may_git_sample/Main.java
@@ -15,7 +15,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Main prin = new Main();
+        prin.printer("Hello World");
+    }
+    
+    public void printer(String message)
+    {
+        System.out.println("The message is " + message);
     }
     
 }
